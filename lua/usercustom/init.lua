ca0838abd1622a94d81d1fd066fd282745c84d35
Sewdohe <sewdohe@divnectar.com>
2022-02-27 19:54:38 -0500

    nvim-tree show ignored files, added doc comment

--- lua/usercustom/init.lua
+++ lua/usercustom/init.lua
@@ -1,4 +1,7 @@
-local custom_okay, custom_visual = pcall(require, "usercustom.visual")
+-- use pcalls in this file to require custom config files,
+-- that way if anyone syncs the repo and doesn't have that file they won't get errors
+
+local custom_okay, _ = pcall(require, "usercustom.visual")
 if not custom_okay then
   return
 end
