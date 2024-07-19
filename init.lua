b175dda373f16e005abf5c0c1adfa8fbc993fd11
Sewdohe <sewdohe@protonmail.com>
2024-07-19 11:03:26 -0400

    temp fix for NvimTree not loading correctly on new version.

--- init.lua
+++ init.lua
@@ -60,7 +60,7 @@ else
 
   --------------------- UI PLUGINS ------------------------------
   --=============================================================
-  require("plugins.ui.nvim-tree") -- File tree for browsing open directory
+  -- require("plugins.ui.filemanager") -- File tree for browsing open directory
   require("plugins.ui.twilight")
   require("plugins.ui.alpha") -- Neovim start page with shortcuts
   require("plugins.ui.legendary") -- Shows a searchable list of key-binds, an ease of use plugin that lets you discover new binds.
@@ -86,6 +86,184 @@ if not nvim_comment_okay then
   return
 end
 nvim_comment.setup()
+
+-- Having to move nvimtree setup outside of the setup-file
+-- for whatever reason it won't load from an external file.
+-- TODO: Figure this out and fix it. I don't like having huge configs inside init.lua.
+require('nvim-tree').setup {
+  auto_reload_on_write = true,
+    create_in_closed_folder = false,
+    disable_netrw = false,
+    hijack_cursor = false,
+    hijack_netrw = true,
+    hijack_unnamed_buffer_when_opening = false,
+    ignore_buffer_on_setup = false,
+    open_on_setup = false,
+    open_on_setup_file = false,
+    open_on_tab = false,
+    sort_by = "name",
+    root_dirs = {},
+    prefer_startup_root = false,
+    update_cwd = false,
+    reload_on_bufenter = false,
+    respect_buf_cwd = false,
+    view = {
+      adaptive_size = false,
+      centralize_selection = false,
+      width = 30,
+      hide_root_folder = false,
+      side = "left",
+      preserve_window_proportions = false,
+      number = false,
+      relativenumber = false,
+      signcolumn = "yes",
+      mappings = {
+        custom_only = false,
+        list = {
+          -- user mappings go here
+        },
+      },
+    },
+    renderer = {
+      add_trailing = false,
+      group_empty = false,
+      highlight_git = true,
+      full_name = false,
+      highlight_opened_files = "none",
+      root_folder_modifier = ":~",
+      indent_markers = {
+        enable = false,
+        icons = {
+          corner = "└ ",
+          edge = "│ ",
+          item = "│ ",
+          none = "  ",
+        },
+      },
+      icons = {
+        webdev_colors = true,
+        git_placement = "before",
+        padding = " ",
+        symlink_arrow = " ➛ ",
+        show = {
+          file = true,
+          folder = true,
+          folder_arrow = true,
+          git = true,
+        },
+        glyphs = {
+          default = "",
+          symlink = "",
+          folder = {
+            arrow_closed = "",
+            arrow_open = "",
+            default = "",
+            open = "",
+            empty = "",
+            empty_open = "",
+            symlink = "",
+            symlink_open = "",
+          },
+          git = {
+            unstaged = "✗",
+            staged = "✓",
+            unmerged = "",
+            renamed = "➜",
+            untracked = "★",
+            deleted = "",
+            ignored = "◌",
+          },
+        },
+      },
+      special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+    },
+    hijack_directories = {
+      enable = true,
+      auto_open = true,
+    },
+    update_focused_file = {
+      enable = false,
+      update_cwd = false,
+      update_root = false,
+      ignore_list = {},
+    },
+    ignore_ft_on_setup = {},
+    system_open = {
+      cmd = "",
+      args = {},
+    },
+    diagnostics = {
+      enable = false,
+      show_on_dirs = false,
+      icons = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+      },
+    },
+    filters = {
+      dotfiles = true,
+      custom = {},
+      exclude = {},
+    },
+    filesystem_watchers = {
+      enable = true,
+      -- interval = 100,
+    },
+    git = {
+      enable = true,
+      ignore = false,
+      timeout = 400,
+    },
+    actions = {
+      use_system_clipboard = true,
+      change_dir = {
+        enable = true,
+        global = false,
+        restrict_above_cwd = false,
+      },
+      expand_all = {
+        max_folder_discovery = 300,
+      },
+      open_file = {
+        quit_on_open = false,
+        resize_window = true,
+        window_picker = {
+          enable = true,
+          chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+          exclude = {
+            filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+            buftype = { "nofile", "terminal", "help" },
+          },
+        },
+      },
+      remove_file = {
+        close_window = true,
+      },
+    },
+    trash = {
+      cmd = "gio trash",
+      require_confirm = true,
+    },
+    live_filter = {
+      prefix = "[FILTER]: ",
+      always_show_folders = true,
+    },
+    log = {
+      enable = false,
+      truncate = false,
+      types = {
+        all = false,
+        config = false,
+        copy_paste = false,
+        diagnostics = false,
+        git = false,
+        profile = false,
+        watcher = false,
+      },
+    },
+}
 
 vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml,*.tsx, *.jsx"
 vim.cmd([[
